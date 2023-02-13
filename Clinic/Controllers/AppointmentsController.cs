4a34e86f4923d9075e4fcf25994a3012a037a99a
hadhoud <hadhoud.abdoo@gmail.com>
2023-02-14 01:52:42 +0200

    add try catch

--- Clinic/Controllers/AppointmentsController.cs
+++ Clinic/Controllers/AppointmentsController.cs
@@ -21,46 +21,82 @@ namespace Clinic.Controllers
         [HttpGet]
         public async Task<List<Appointment>> GetAppointmentListAsync()
         {
-            var appointment = await mediator.Send(new GetAppointmentListQuery());
-
-            return appointment;
+            try
+            {
+                var appointments = await mediator.Send(new GetAppointmentListQuery());
+                return appointments;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         [HttpGet("appointmentId")]
         public async Task<Appointment> GetAppointmentByIdAsync(int appointmentId)
         {
-            var appointment = await mediator.Send(new GetAppointmentByIdQuery() { Id = appointmentId });
+            try
+            {
+                var appointment = await mediator.Send(new GetAppointmentByIdQuery() { Id = appointmentId });
 
-            return appointment;
+                return appointment;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         [HttpPost]
         public async Task<Appointment> AddAppointmentAsync(Appointment appointment)
         {
-            var appointmentEntity = await mediator.Send(new CreateAppointmentCommand(
-                appointment.Name,
-                appointment.PhoneNumber,
-                appointment.Note,
-                appointment.AppointmentDate));
-            return appointmentEntity;
+            try
+            {
+
+                var appointmentEntity = await mediator.Send(new CreateAppointmentCommand(
+                    appointment.Name,
+                    appointment.PhoneNumber,
+                    appointment.Note,
+                    appointment.AppointmentDate));
+                return appointmentEntity;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         [HttpPut]
         public async Task<int> UpdateAppointmentAsync(Appointment appointment)
         {
-            var isAppointmentUpdated = await mediator.Send(new UpdateAppointmentCommand(
-               appointment.Id,
-               appointment.Name,
-               appointment.PhoneNumber,
-               appointment.Note,
-               appointment.AppointmentDate));
-            return isAppointmentUpdated;
+            try
+            {
+
+                var isAppointmentUpdated = await mediator.Send(new UpdateAppointmentCommand(
+                   appointment.Id,
+                   appointment.Name,
+                   appointment.PhoneNumber,
+                   appointment.Note,
+                   appointment.AppointmentDate));
+                return isAppointmentUpdated;
+            }
+            catch (Exception ex)
+            {
+                return default;
+            }
         }
 
         [HttpDelete]
         public async Task<int> DeleteAppointmentAsync(int Id)
         {
-            return await mediator.Send(new DeleteAppointmentCommand() { Id = Id });
+            try
+            {
+                return await mediator.Send(new DeleteAppointmentCommand() { Id = Id });
+            }
+            catch (Exception ex)
+            {
+                return default;
+            }
         }
     }
 }
